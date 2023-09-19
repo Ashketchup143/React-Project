0990c59c5c720bbe32ae91ebb454de127083ec57
Ashketchup143 <anmlim@addu.edu.ph>
2023-09-19 19:37:10 +0800

    hehe first input

---
+++ src/components/Checkbox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Checkbox = () => {
+  return (
+    <div>
+      <input type='checkbox'/>
+      <span>Only show Products in stock</span>
+    </div>
+  )
+}
+
+export default Checkbox
